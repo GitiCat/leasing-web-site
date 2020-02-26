31a41a864451e9207ef1795783084edfe1fa7052
Ivan Zhuravlev <ivan_zhuravlev_1997@mail.ru>
2020-02-26 16:49:33 +0300

    dev-anims

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -6,17 +6,25 @@ document.addEventListener("DOMContentLoaded", () => {
     navScrollingToElement("link-objects", "objects");
     navScrollingToElement("link-conditions", "conditions");
     navScrollingToElement("link-contacts", "contacts");
+
+    objectsOpen();
+
+    var nav = document.getElementById("nav");
+    nav.style.cssText = "margin-top: 92px";
+
 });
 
 window.addEventListener("scroll", () => {
     var nav = document.getElementById("nav");
 
-    if(Number(window.scrollY) > 45) {
-        if(!nav.classList.contains('scroll-nav')) {
-            nav.classList.add("scroll-nav");
-        }    
+    if (window.pageYOffset < 5) {
+        nav.style.cssText = "margin-top: 92px; \
+            background-color: transparent; \
+            box-shadow: none";
     } else {
-        nav.classList.remove("scroll-nav");
+        nav.style.cssText = "margin-top: 0; \
+        background-color: #212121; \
+        box-shadow: 0 2px 4px rgba(0,0,0,.5);";
     }
 });
 
@@ -52,11 +60,61 @@ function navScrollingToElement(link, to) {
     var element = document.getElementById(link);
     var block = document.getElementById(to);
 
+    var V = .3;
+
     element.addEventListener("click", (event) => {
-        block.scrollIntoView({
-            block: "start",
-            behavior: "smooth"
-        });
+
+        var w = window.pageYOffset,
+            t = block.getBoundingClientRect().top - 60,
+            start = null;
+
+        requestAnimationFrame(step);
+
+        function step(time) {
+            if (start === null) start = time;
+            var progress = time - start,
+                r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+
+            window.scrollTo(0, r);
+
+            if (r != w + t) {
+                requestAnimationFrame(step);
+            }
+        }
+
         event.preventDefault();
     });
+}
+
+function objectsOpen() {
+    var objectsList = document.querySelectorAll(".l-o-section");
+    var openingClass = "opening";
+
+    objectsList.__proto__.opening = undefined;
+
+    objectsList.__proto__.closeOpening = function() {
+
+        if(objectsList.opening !== undefined)
+            objectsList.opening.classList.remove(openingClass)
+    }
+
+    objectsList.__proto__.open = function(obj) {
+        var current = objectsList.opening,
+            open = obj;
+
+        if (current !== undefined)
+            current.classList.remove(openingClass);
+
+        if (open === undefined)
+            throw new Error('ObjectList: Objects to open is not defined!');
+
+        open.classList.add(openingClass);
+        objectsList.opening = open;
+    }
+
+    objectsList.forEach(obj => {
+        obj.addEventListener("click", (e) => {
+            objectsList.open((e.target.offsetParent).offsetParent);
+        });
+    })
 }
--- src/views/index/components/objects.php
+++ src/views/index/components/objects.php
@@ -14,6 +14,60 @@
                 лизинг. Данная услуга позволяет взять в аренду с последующим
                 правом выкупа движимое и недвижимое имущество.
             </p>
+            <div class="l-objects">
+                <div class="l-o-section">
+                    <div class="l-o-image" id="gruz">
+                        <div class="content">
+                            <div class="text">
+                                <h2>Автомобили</h2>
+                                <span>Легковые и грузовые автомобили</span>
+                            </div>
+                            <div class="back" id="back">
+                                <i class="material-icons">keyboard_return</i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="l-o-section">
+                    <div class="l-o-image" id="esk">
+                        <div class="content">
+                            <div class="text">
+                                <h2>Сцетехника</h2>
+                                <span>Техника для строительства</span>
+                            </div>
+                            <div class="back" id="back">
+                                <i class="material-icons">keyboard_return</i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="l-o-section">
+                    <div class="l-o-image" id="stroy">
+                        <div class="content">
+                            <div class="text">
+                                <h2>Прочие работы</h2>
+                                <span>Техника для сельского хозяйства и дорожных работ</span>
+                            </div>
+                            <div class="back" id="back">
+                                <i class="material-icons">keyboard_return</i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="l-o-section">
+                    <div class="l-o-image" id="prom">
+                        <div class="content">
+                            <div class="text">
+                                <h2>Промышленность</h2>
+                                <span>Промышленное оборудование и коммерческая недвижимость</span>
+                            </div>
+                            <div class="back" id="back">
+                                <i class="material-icons">keyboard_return</i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <p>
                 Лизинг - удобный способ начать предпринимательскую деятельность
                 в ограниченный период времени. На компания предлагает следующие объекты
